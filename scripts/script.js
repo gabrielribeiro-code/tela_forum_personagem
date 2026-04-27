a044def46fcc18f414172fa3d085a43224dde55b
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-04-27 09:36:13 -0300

    feat: fizemos todo o sistema de javascript para a tela de login, assim tendo uma senha e usuario fixo.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+let cadastro = false;
+
+
 let form = document.getElementById('form-login');
 
 form.addEventListener('submit', function(e){
@@ -10,5 +13,32 @@ form.addEventListener('submit', function(e){
 
     let nome = document.getElementById('nome').value;
     let senha = document.getElementById('senha').value;
+    let mensagem = document.getElementById("mensagem");
 
+    mensagem.innerHTML = "";
+
+    if(nome ===  "" || senha === "" ){
+        mensagem.innerHTML = "<div class = 'erro' <p> Prencha todos os campos! </p> </div>";
+        return;
+    }
+
+    const usuarioCorreto = "barbabranca";
+    const senhaCorreta = "barbabranca1234";
+
+    if(nome === usuarioCorreto && senha === senhaCorreta) {
+
+         mensagem.innerHTML = "<div class = 'sucesso' <p> Login feito com Sucesso! </p> </div>";
+         //Aqui é caso o usuario coloque o nome correto barbabranca;
+
+    }else{
+
+         mensagem.innerHTML = "<div class = 'erro' <p> Dados Errados! Usuário e Senha estão errados! </p> </div>";
+    }
+
+    
+
+    
+document.getElementById("form-login").reset();
+
+        
 })
