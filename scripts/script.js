aabf2dfce80f0866c106bbbf53d1c10a92501b95
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-04-27 08:17:11 -0300

    add - página assets e image

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+let form = document.getElementById('form-login');
+
+form.addEventListener('submit', function(e){
+
+
+    e.preventDefault();
+
+
+    let valido = true;
+
+    let nome = document.getElementById('nome').value;
+    let senha = document.getElementById('senha').value;
+
+})
