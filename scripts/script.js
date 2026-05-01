e27201d59cb1cd927694af218d754f218a5896ab
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-05-01 16:38:38 -0300

    fix: correção de erros ao dar login e nao ir para outra tela.

--- scripts/script.js
+++ scripts/script.js
@@ -27,14 +27,14 @@ form.addEventListener('submit', function(e){
 
     if(nome === usuarioCorreto && senha === senhaCorreta) {
 
-         mensagem.innerHTML = "<div class = 'sucesso' <p> Login feito com Sucesso! </p> </div>";
+         mensagem.innerHTML = "<div class = 'sucesso'> <p> Login feito com Sucesso! </p> </div>";
          //Aqui é caso o usuario coloque o nome correto barbabranca;
 
-         window.location.replace("file:///C:/Users/gabriel_ribeiro16/Desktop/tela_forum_personagem/public/main.html#");
+         window.location.href = "https://gabrielribeiro-code.github.io/tela_forum_personagem/";
 
     }else{
 
-         mensagem.innerHTML = "<div class = 'erro' <p> Dados Errados! Usuário e Senha estão errados! </p> </div>";
+         mensagem.innerHTML = "<div class = 'erro'> <p> Dados Errados! Usuário e Senha estão errados! </p> </div>";
     }
 
     
