0d703663c886ce9598b0e6774c291bb74fc5bfeb
devcaetan0 <davi_caetano@estudante.sesisenai.org.br>
2026-05-02 17:59:58 -0300

    style - identação do código

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ let cadastro = false;
 
 let form = document.getElementById('form-login');
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
 
 
     e.preventDefault();
@@ -17,7 +17,7 @@ form.addEventListener('submit', function(e){
 
     mensagem.innerHTML = "";
 
-    if(nome ===  "" || senha === "" ){
+    if (nome === "" || senha === "") {
         mensagem.innerHTML = "<div class = 'erro' <p> Prencha todos os campos! </p> </div>";
         return;
     }
@@ -25,32 +25,32 @@ form.addEventListener('submit', function(e){
     const usuarioCorreto = "barbabranca";
     const senhaCorreta = "barbabranca1234";
 
-    if(nome === usuarioCorreto && senha === senhaCorreta) {
+    if (nome === usuarioCorreto && senha === senhaCorreta) {
 
-         mensagem.innerHTML = "<div class = 'sucesso'> <p> Login feito com Sucesso! </p> </div>";
-         //Aqui é caso o usuario coloque o nome correto barbabranca;
+        mensagem.innerHTML = "<div class = 'sucesso'> <p> Login feito com Sucesso! </p> </div>";
+        //Aqui é caso o usuario coloque o nome correto barbabranca;
 
-         window.location.href = "https://gabrielribeiro-code.github.io/tela_forum_personagem/public/main.html";
+        window.location.href = "https://gabrielribeiro-code.github.io/tela_forum_personagem/public/main.html";
 
-    }else{
+    } else {
 
-         mensagem.innerHTML = "<div class = 'erro'> <p> Dados Errados! Usuário e Senha estão errados! </p> </div>";
+        mensagem.innerHTML = "<div class = 'erro'> <p> Dados Errados! Usuário e Senha estão errados! </p> </div>";
     }
 
-    
 
-    
-document.getElementById("form-login").reset();
+
+
+    document.getElementById("form-login").reset();
 });
 
 //lógica para criar um comentário na aba de discussões
 
-function enviarComentario(){
+function enviarComentario() {
 
     let texto = document.getElementById("comentario").value;
     //aqui nos pegamos o texto da outra pagina pelo id
 
-    if(texto =="") return; 
+    if (texto == "") return;
     // aqui se nao colocarem nada, faz nada
 
     let novo = document.createElement("p");
@@ -60,12 +60,12 @@ function enviarComentario(){
     //aqui colocamos o texto dentro
 
     document.getElementById("area-comentarios").appendChild(novo);
-// aqui é para adicionar na tela 
-// O appendChild serve justamente para jogar na tela
+    // aqui é para adicionar na tela 
+    // O appendChild serve justamente para jogar na tela
 
-document.getElementById("comentario").value = "";
-// aqui é para limpar o comentário.
+    document.getElementById("comentario").value = "";
+    // aqui é para limpar o comentário.
 
 }
 
-        
+
